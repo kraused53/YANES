27063b401c72c97cc2eab0dac0d006f2df403d3c
kraused53 <kraused53@gmail.com>
2026-07-10 15:06:27 -0400

    testing testing

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DummyTesting {
+public class DummyTest {
 
     @Test
     void dummyPass() {
