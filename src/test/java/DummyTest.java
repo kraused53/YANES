4224df948838f9a411f59e733ebf7c1bacafe50d
kraused53 <kraused53@gmail.com>
2026-07-10 15:09:15 -0400

    Test to watch it catch a failed test

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,10 +1,18 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DummyTest {
 
     @Test
+    @DisplayName("This test will pass!")
     void dummyPass() {
         assertEquals(true, true);
+    }
+
+    @Test
+    @DisplayName("This test will fail...")
+    void dummyFail() {
+        assertEquals(false, true);
     }
 }
